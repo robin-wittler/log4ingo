8b6f98815a4da016edc5c608db9e742dd70c322f
Robin Wittler <real@the-real.org>
2011-11-28 18:40:01 +0100

    initial debian handling

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from distutils.core import setup
+
+__author__ = 'real'
+__email__ = '%s@the-real.org' % (__author__)
+
+setup(
+    name='log4ingo',
+    version='0.1.0',
+    description='a log module dedicated to a guy named ingo',
+    author='Robin Wittler',
+    author_email='real@the-real.org',
+    py_modules=['log4ingo'],
+)
+
+if __name__ == '__main__':
+    pass
